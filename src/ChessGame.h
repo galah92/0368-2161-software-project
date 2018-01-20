e18102df73af6b58ff86a1d0520b71edfa998d74
galah92 <galaharoni92@gmail.com>
2018-01-20 11:08:37 +0200

    chess: Added ideal gameloop (commented), ChessGame struct modifications

--- src/ChessGame.h
+++ src/ChessGame.h
@@ -1,6 +1,8 @@
 #ifndef CHESS_GAME_H_
 #define CHESS_GAME_H_
 
+#include "FSAStack.h"
+
 #define CHESS_GAME_MODE_1_PLAYER 1
 #define CHESS_GAME_MODE_2_PLAYER 2
 #define CHESS_GAME_USER_COLOR_BLACK 1
@@ -11,6 +13,12 @@
 typedef enum {
     CHESS_GAME_SUCCESS,
     CHESS_GAME_INVALID_ARGUMENT,
+    CHESS_GAME_INVALID_POSITION,
+    CHESS_GAME_EMPTY_POSITION,
+    CHESS_GAME_ILLEGAL_MOVE,
+    CHESS_GAME_KING_IS_STILL_THREATENED,
+    CHESS_GAME_KING_WILL_BE_THREATENED,
+
 } CHESS_GAME_RESULT;
 
 typedef struct {
@@ -23,7 +31,18 @@ typedef struct {
     ChessGameSettings settings;
     char board[CHESS_GAME_BOARD_SIZE][CHESS_GAME_BOARD_SIZE];
     int currentPlayer;
+    FSAStack *history;
 } ChessGame;
+
+typedef struct {
+    unsigned char x;
+    unsigned char y;
+} ChessBoardPos;
+
+typedef struct {
+    ChessBoardPos from;
+    ChessBoardPos to;
+} ChessMove;
 
 
 ChessGame* ChessGame_Create();
@@ -41,6 +60,12 @@ CHESS_GAME_RESULT ChessGame_SetUserColor(ChessGame *game, int userColor);
 CHESS_GAME_RESULT ChessGame_PrintSettings(ChessGame *game);
 
 CHESS_GAME_RESULT ChessGame_GetSettingsString(ChessGame *game, char *outString);
+
+CHESS_GAME_RESULT ChessGame_IsLegalMove(ChessGame *game, ChessMove move);
+
+CHESS_GAME_RESULT ChessGame_DoMove(ChessGame *game, ChessMove move);
+
+CHESS_GAME_RESULT ChessGame_GetMoves(ChessGame *game, ChessBoardPos pos, ChessMove *moves[]);
 
 
 #endif
--- src/chessprog.c
+++ src/chessprog.c
@@ -1,75 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include "ChessGame.h"
-#include "UserCommand.h"
+#include "UIManager.h"
 
-
-typedef enum {
-    SETTINGS_STATE,
-    GAME_STATE,
-    QUIT,
-    RESTARTED,
-} AppState;
-
-
-void HandleSettingsState(ChessGame *game) {
-    printf("Specify game settings or type 'start' to begin a game with the current settings:\n");
-    UserCommand command;
-    while (command.type != USER_COMMAND_START) {
-        // TODO: update 'command' from user input
-        switch (command.type) {
-            case USER_COMMAND_INVALID:
-                printf("ERROR: invalid command\n");
-                break;
-            case USER_COMMAND_GAME_MODE:
-                ChessGame_SetGameMode(game, command.params[0]);
-                char *mode = command.params[0] == CHESS_GAME_MODE_1_PLAYER ? "1-player" : "2-player";
-                printf("Game mode is set to %s\n", mode);
-                break;
-            case USER_COMMAND_DIFFICULTY:
-                ChessGame_SetDifficulty(game, command.params[0]);
-                break;
-            case USER_COMMAND_USER_COLOR:
-                ChessGame_SetUserColor(game, command.params[0]);
-                break;
-            case USER_COMMAND_LOAD_GAME:
-                // TODO: implement
-                break;
-            case USER_COMMAND_DEFAULT_SETTINGS:
-                ChessGame_SetDefaultSettings(game);
-                break;
-            case USER_COMMAND_PRINT_SETTINGS:
-                ChessGame_PrintSettings(game);
-                break;
-            case USER_COMMAND_QUIT:
-                break;
-            case USER_COMMAND_START:
-                printf("Starting game…\n");
-                return;
-            case USER_COMMAND_MOVE: break;
-            case USER_COMMAND_GET_MOVES: break;
-            case USER_COMMAND_SAVE: break;
-            case USER_COMMAND_UNDO: break;
-            case USER_COMMAND_RESET: break;
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
-    AppState state = SETTINGS_STATE;
-    // ChessGame *game;
-    if (argc > 1 && strcmp(argv[1], "-g") == 0) {
-        printf("GUI mode - WIP\n");
-    } else {
-        printf(" Chess\n-------\n");
-        while (state != QUIT) {
-            if (state == RESTARTED) {
-                // game = ChessGame_Create();
-                state = GAME_STATE;
-            } else {
-
-            }
-        }
-    }
+    UIManager *uiManager = UIManager_Create(argc, argv);
+    // GameManager *gameManager = GameManager_Create();
+    // UserCommand *command;
+    // while (1) {
+    //     command = UIManager_ProcessInput(uiManager);
+    //     GameManager_ProcessCommand(gameManager, command);
+    //     UIManager_Render(uiManager, gameManager->gameState);
+    // }
+    UIManager_Destroy(uiManager);
+    // GameManager_Destroy(gameManager);
     return 0;
 }
